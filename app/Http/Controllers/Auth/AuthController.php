8b3726e6a0815e235f9629a79f93104dfdb453ea
Shafira Hasna Fauzi <shafirahasna32@gmail.com>
2026-03-09 22:32:52 +0700

    frontend login page

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
 
             if(auth()->user()->role === 'admin'){
                 return redirect('/admin');
-            }
+            } 
             return redirect('/');
         }
 
---
+++ app/Http/Controllers/CalonAdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class CalonAdminController extends Controller
+{
+    public function index(){
+        
+    }
+}
