4cd5773744e77c42cec23949ef8d60745ada99cf
rikiulirniam <rikiulir@gmail.com>
2026-03-09 20:53:59 +0700

    feat: add CRUD calon admin with photo upload

---
+++ app/Http/Controllers/CalonAdminController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CalonAdmin;
+use App\Http\Requests\StoreCalonAdminRequest;
+use App\Http\Requests\UpdateCalonAdminRequest;
+use Illuminate\Support\Facades\Storage;
+
+class CalonAdminController extends Controller
+{
+    public function index()
+    {
+        return view("pages.admin.camin.index", [
+            "camin" => CalonAdmin::orderBy('no_urut')->get()
+        ]);
+    }
+
+    public function create()
+    {
+        return view("pages.admin.camin.create");
+    }
+
+    public function store(StoreCalonAdminRequest $request)
+    {
+        $data = $request->validated();
+
+        if ($request->hasFile('foto')) {
+            $data['foto'] = $request->file('foto')->store('calon_admins', 'public');
+        }
+
+        CalonAdmin::create($data);
+
+        return redirect()->route('camin.index')->with('success', 'Calon admin berhasil ditambahkan.');
+    }
+
+    public function show(CalonAdmin $calonAdmin)
+    {
+        //
+    }
+
+    public function edit(CalonAdmin $camin)
+    {
+        return view("pages.admin.camin.edit", compact('camin'));
+    }
+
+    public function update(UpdateCalonAdminRequest $request, CalonAdmin $camin)
+    {
+        $data = $request->validated();
+
+        if ($request->hasFile('foto')) {
+            if ($camin->foto) {
+                Storage::disk('public')->delete($camin->foto);
+            }
+            $data['foto'] = $request->file('foto')->store('calon_admins', 'public');
+        } else {
+            unset($data['foto']);
+        }
+
+        $camin->update($data);
+
+        return redirect()->route('camin.index')->with('success', 'Calon admin berhasil diperbarui.');
+    }
+
+    public function destroy(CalonAdmin $camin)
+    {
+        if ($camin->foto) {
+            Storage::disk('public')->delete($camin->foto);
+        }
+
+        $camin->delete();
+
+        return redirect()->route('camin.index')->with('success', 'Calon admin berhasil dihapus.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Middleware\Authorize;
 use App\Models\User;
@@ -17,6 +18,4 @@ Route::prefix('auth')->group(function(){
 
 });
 
-Route::get("/admin", function(){
-    return view("pages.admin.dashboard");
-})->middleware("auth", Authorize::class);
+Route::resource("/admin", AdminController::class)->middleware("auth", Authorize::class);
