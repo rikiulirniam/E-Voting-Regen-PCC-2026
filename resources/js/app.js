adc31d3e2c697e4f35aa6c534b567f62bf52faeb
Algaray02 <alfin.rozzaq.270206@gmail.com>
2026-04-07 10:21:33 +0700

    fix: bug in frontend of dashboard admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,3 +20,36 @@ document.addEventListener('DOMContentLoaded', function () {
         submitBtn.textContent = 'Loading...';
     });
 });
+
+// logic untuk proses animasi masuk website
+document.addEventListener("DOMContentLoaded", () => {
+    const introScreen = document.getElementById("intro-screen");
+    const introLogo = document.getElementById("intro-logo");
+    const sudahIntro = localStorage.getItem("introSeen");
+
+    if (introScreen && introLogo) {
+        if (!sudahIntro) {
+            document.body.classList.add('no-scroll');
+            setTimeout(() => {
+                introLogo.classList.add('in-logo');
+                if (typeof introLoad !== 'undefined' && introLoad) introLoad.style.width = '100%';
+            }, 300);
+
+            setTimeout(() => {
+                introLogo.classList.remove('in-logo');
+                introLogo.classList.add('out-logo');
+
+                introScreen.style.opacity = '0';
+
+                setTimeout(() => {
+                    introScreen.style.display = 'none';
+                    document.body.classList.remove('no-scroll');
+                    
+                }, 800);
+                localStorage.setItem('introSeen', 'true');
+            }, 2300);
+        } else {
+            introScreen.style.display = 'none';
+        }
+    }
+});
